1a48846125f900606fc58eeff9e09b728e430845
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2020-01-19 14:07:35 +0530

    Re-factor header style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from "@reach/router"
 
 type HeaderProps = {
     title: string;
@@ -7,9 +9,30 @@ type HeaderProps = {
 }
 
 export const Header = ({ title , isActive = true }: HeaderProps) => (
-    <div className="App-header">
+    <StyledHeader>
         <h1 className="App-header__title">
             {title}
         </h1>
-    </div>
+        <nav>
+          {/* <Link to="/">Home</Link> |{" "} */}
+        </nav>
+    </StyledHeader>
 )
+
+const StyledHeader = styled.div`
+    /* position: fixed; */
+    width: 100%;
+    min-height: 10vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: #4a5568;
+      /* background:
+  radial-gradient(circle at 100% 50%, transparent 20%, #4a5568 21%,
+  #4a5568 34%, transparent 35%, transparent),
+  radial-gradient(circle at 0% 50%, transparent 20%, #4a5568 21%, #4a5568 34%, transparent 35%, transparent) 0 -50px;
+background-color: #2d3748;
+background-size: 75px 100px; */
+`;
