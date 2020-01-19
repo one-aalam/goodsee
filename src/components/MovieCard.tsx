ac7953db80812ee5ce2c1dafc647c4957043b569
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2020-01-19 13:52:18 +0530

    Enhance styling for MovieCard

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { Link } from '@reach/router'
 import styled from 'styled-components';
+
 import { ITMDBMovie, POSTER_PATH } from '../hooks/tmdb';
 
 type MovieCardProps = {
@@ -9,11 +11,19 @@ type MovieCardProps = {
 export const MovieCard :React.FC<MovieCardProps> = ({ movie, children }) => {
     return (
         <StyledMovieCard>
-           <img alt={movie.title} src={`${POSTER_PATH}${movie.poster_path}`} />
+            <Link to={`movie/${movie.id}`}>
+                <img alt={movie.title} src={`${POSTER_PATH}${movie.poster_path}`} />
+           </Link>
         </StyledMovieCard>
     )
 }
 
-const StyledMovieCard = styled.div`
-    box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
+export const StyledMovieCard = styled.div`
+    img {
+        border-radius: .25rem;
+        box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04);
+        &:hover {
+            box-shadow: 0 25px 50px -12px rgba(0, 0, 0, 0.25);
+        }
+    }
 `
